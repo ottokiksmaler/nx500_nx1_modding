a9822d6227d0934cbb979b6a09b44720863007e7
ottokiksmaler <ottokiksmaler@users.noreply.github.com>
2016-05-03 10:42:08 +0200

    Fixed two possible minor bugs
    EV get force-released in app when starting external script.
    Sleep detect does not run when running from SD card as keyscan is killed by system either way.

--- keyscan.c
+++ keyscan.c
@@ -7,9 +7,13 @@
  * If the fourth parameter is "debug" verbose mode is turned on. File named /tmp/key_code holds name of key currently pressed
  * If the fourth parameter is "bbaf" then "True BB-AF" mode is turned on on NX1 - CAF while AF pressed, MF when not.
  * 
+ * keyscan will try to execute files by {scripts}/auto/*.sh in order they are found in upon star or restart (wake up)
+ * 
+ * If there is no /mnt/mmc/scripts directory it will try to use /opt/usr/devel/scripts directory.
+ * 
  * Default command line: keyscan /dev/event0 /dev/event1 /mnt/mmc/scripts/ [debug|bbaf]
  * 
- * Compile: arm-linux-gnueabihf-gcc --static -o keyscan keyscan.c -s
+ * Compile: arm-linux-gnueabi-gcc --static -o keyscan keyscan.c -lpthread -s
  * 
  */
 #include <stdlib.h>
@@ -22,6 +26,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/file.h>
+#include <signal.h>
+#include <pthread.h>
+#include <glob.h>
+
+#define timer_sleep 1 // time in seconds to detect sleep events
 
 #define NXKEY_EV 173
 #define NXKEY_OK 96
@@ -78,8 +87,17 @@ static const char *const evval[3] = {
     "REPEATED"
 };
 
-int debug=0, bbaf=0, bbaf_nx500=0;
-static const char *key_temp_file="/tmp/key_code";
+int debug=0, bbaf=0, bbaf_nx500=0, running_from_card=0;
+static const char *key_temp_file="/tmp/key_code"; // file to store current key name in debugging
+static const char *opt_scripts_dir="/opt/usr/devel/scripts/"; // default scripts directory if none found on SD card
+static char *scripts_dir;
+
+pthread_t timer_thread, cleanup_thread;
+
+int execute_script(char * script_name) {
+	debug && printf("Executing: %s\n",script_name);
+	return system(script_name);
+}
 
 long msec_passed(struct timeval *fromtime, struct timeval *totime)
 {
@@ -96,21 +114,85 @@ void create_temp_file(char *key) {
 	close(fd);
 }
 
-int main (int argc, char *argv[])
-{
-	// Prevent double starting the keyscan
-	int pid_file = open("/tmp/keyscan.pid", O_CREAT | O_RDWR, 0666);
-	if(flock(pid_file, LOCK_EX | LOCK_NB)) {
-		if(EWOULDBLOCK == errno) {
-			printf("Error - another %s instance already running!\n",argv[0]);
-			return 255;
+/* auto_run_process
+ * 
+ * Executes scripts from {scripts}/auto/*.sh 
+ * 
+ * Sleeps 2s for scripts on SD card
+ * 
+ */
+void auto_run_process() {
+	char *auto_dir, *command;
+	int i;
+	debug && printf("Executing auto scripts\n");
+	asprintf(&auto_dir,"%s/auto/*.sh",scripts_dir);
+	// need some time for files to become accessible on SD card
+	if (strstr(auto_dir,"mmc") || strstr(auto_dir,"sdcard")) {
+		sleep(2);
+	}
+	// blink SD card led on back to indicate auto running scripts
+	for (i=0;i<3*debug;i++)
+		execute_script("echo 1 > /sys/devices/platform/leds-gpio/leds/card/brightness;sleep 0.2;echo 0 > /sys/devices/platform/leds-gpio/leds/card/brightness; sleep 0.1");
+	glob_t globbuf;
+	if (GLOB_NOMATCH != glob( auto_dir, 0, NULL, &globbuf)) {
+		for( i = 0; i < globbuf.gl_pathc; i++ ) {
+			asprintf(&command, "/opt/usr/devel/scripts/popup_timeout \"X: %s\" 1", globbuf.gl_pathv[i]);
+			debug && system(command);
+			execute_script(globbuf.gl_pathv[i]);
 		}
 	}
+	if( globbuf.gl_pathc > 0 )
+		globfree( &globbuf );
 	
+}
 
+// Detect sleep events
+void* timer_loop(void* arg) {
+	struct timeval previous_time_loop, current_time_loop;
+	long msec_elapsed;
+	while(1) {
+		gettimeofday(&previous_time_loop, NULL); 
+		sleep(timer_sleep);
+		gettimeofday(&current_time_loop, NULL); 
+		msec_elapsed = msec_passed(&previous_time_loop,&current_time_loop);
+		if (msec_elapsed>(timer_sleep*1000+300)) auto_run_process();
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	// Prevent double starting the keyscan. If the lock file is stale - remove/reinsert battery // TODO
+	int pid_file = open("/tmp/keyscan.pid", O_CREAT | O_RDWR, 0666);
+	int old_pid;
+	if (pid_file) {
+		read(pid_file, &old_pid, sizeof(old_pid));
+		printf("OLD PID: %d\n",old_pid);
+		if(flock(pid_file, LOCK_EX | LOCK_NB)) {
+			if(EWOULDBLOCK == errno) {
+				printf("Error - another %s instance already running!\n",argv[0]);
+				return 255;
+			}
+			printf("Error locking the pid file: %d\n",errno);
+			return errno;
+		}
+		int pid=(int)getpid();
+		printf("PID: %d\n",pid);
+		write(pid_file,&pid, sizeof(pid));
+		fsync(pid_file);
+	} else {
+		printf("Unable to create PID file.\n");
+		return 255;
+	}
+
+	// if running from card no need for sleep detect - keyscan is killed on sleep
+	if (strstr(argv[0],"mmc") || strstr(argv[0],"sdcard")) {
+		running_from_card=1;
+	}
+	if(0 == running_from_card)
+		pthread_create(&timer_thread, NULL, &timer_loop, NULL);
+	
     char *input_device0 = NULL;
 	char *input_device1 = NULL;
-	char *shell_dir = NULL;
     int fd0,fd1;
 	fd_set inputs;
     struct input_event ev, previous_ev = {};
@@ -118,7 +200,7 @@ int main (int argc, char *argv[])
 	long msec_elapsed;
     ssize_t n;
 	short unsigned int ev_pressed=0;
-	char shell_name[255], shell_script[255];
+	char shell_name[255], shell_script[255], opt_shell_script[255];
 	int call_shell=0;
 	char *nxkeyname[256];
 	int i,ready;
@@ -172,18 +254,27 @@ int main (int argc, char *argv[])
 	}
 	
 	if (argc > 3)
-		shell_dir = argv[3];
-	else 
-		shell_dir = "/mnt/mmc/scripts/";
-
-	if (argc > 4) {
-		if (strcmp("bbaf",argv[4])==0) {
-			printf("BB-AF ON\n");
-			bbaf=1;
+		scripts_dir = argv[3];
+	else
+		scripts_dir = "/mnt/mmc/scripts/";
+	
+	if (0 != access(scripts_dir,X_OK)) {
+		if (0 == access(opt_scripts_dir,X_OK)) {
+			asprintf(scripts_dir,"%s",opt_scripts_dir);
 		} else {
-			printf("DEBUG ON\n");
-			debug=1;
+			printf("Invalid scripts directory: %s\nExiting the program...\n",scripts_dir);
+			exit(255);
 		}
+	}
+
+	if (strcmp(argv[argc - 1], "debug") == 0) {
+		printf("DEBUG ON\n");
+		debug = 1;
+	}
+
+	if (strcmp(argv[argc - 1], "bbaf") == 0) {
+		printf("BB-AF ON\n");
+		bbaf=1;
 	}
 
 	debug && printf("Opening inputs %s %s\n",input_device0,input_device1) && fflush(stdout);
@@ -201,6 +292,9 @@ int main (int argc, char *argv[])
         return EXIT_FAILURE;
     }
     gettimeofday(&previous_press_time, NULL); 
+	// TODO should we identify mods?
+	//system("/opt/usr/devel/scripts/popup_timeout '  NX-MOD v2.0  ' 1");
+    auto_run_process(); // execute all automatic scripts on first start
     while (1) {
 		FD_ZERO(&inputs);
 		FD_SET(fd0,&inputs);
@@ -236,15 +330,13 @@ int main (int argc, char *argv[])
 			}
 			if (NXKEY_EV1 != (int)ev.code && msec_elapsed < 1000 && ev.code == previous_ev.code && ev.value == previous_ev.value) {
 				debug && printf("Doubleclick %s %d\n", nxkeyname[(int)ev.code], (int)ev.code);
-				strncpy(shell_name,nxkeyname[(int)ev.code],8);
-				strncat(shell_name,"_",8);
-				strncat(shell_name,nxkeyname[(int)ev.code],8);
+				sprintf(shell_name,"%s_%s",nxkeyname[(int)ev.code],nxkeyname[(int)ev.code]);
 				call_shell=1;
 			}
 			if (bbaf && NXKEY_AFON == (int)ev.code && 1 == ev.value) {
-				system("/usr/bin/st cap capdtm setusr AFMODE 0x70001");
+				execute_script("/usr/bin/st cap capdtm setusr AFMODE 0x70001");
 			} else if (bbaf && NXKEY_AFON == (int)ev.code && 0 == ev.value) {
-				system("/usr/bin/st cap capdtm setusr AFMODE 0x70003");
+				execute_script("/usr/bin/st cap capdtm setusr AFMODE 0x70003");
 			}
 			if (NXKEY_EV == (int)ev.code || NXKEY_EV1 == (int)ev.code || NXKEY_SHIFT == (int)ev.code) {
 				if (1 == ev.value)
@@ -253,28 +345,31 @@ int main (int argc, char *argv[])
 					ev_pressed=0;
 			}
 			if (ev_pressed == 1 && (int)ev.code != NXKEY_EV && (int)ev.code != NXKEY_SHIFT && 1 == (int)ev.value) {
+				ev_pressed=0;
 				debug && printf("Combo EV + %s %d %s\n", nxkeyname[(int)ev.code], (int)ev.code, evval[(int)ev.value]);
-				strncpy(shell_name,"EV_",4);
-				strncat(shell_name,nxkeyname[(int)ev.code],16);
+				sprintf(shell_name,"EV_%s",nxkeyname[(int)ev.code]);
 				call_shell=1;
 			}
 			if (ev.value == 1) {
 				previous_ev = ev;
 			}
 			if (1 == call_shell && strlen(shell_name)>4) {
-				strncpy(shell_script,shell_dir,100);
-				strncat(shell_script,shell_name,16);
-				strncat(shell_script,".sh",3);
+				sprintf(shell_script,"%s/%s.sh",scripts_dir,shell_name);
 				if( access( shell_script, X_OK ) != -1 ) {
-					debug && printf("Executing file '%s'\n",shell_script);
-					system(shell_script);
+					execute_script(shell_script);
 				} else {
-					debug && printf("No such file '%s'\n",shell_script);
+					sprintf(opt_shell_script,"%s/%s.sh",opt_scripts_dir,shell_name);
+					if( access( opt_shell_script, X_OK ) != -1 ) {
+						execute_script(opt_shell_script);
+					} else {
+						debug && printf("No such file '%s' or '%s'\n",shell_script,opt_shell_script);
+					}
 				}
 			}
 		}
     }
     fflush(stdout);
-    fprintf(stderr, "%s.\n", strerror(errno));
+    fprintf(stderr, "Error: %s.\n", strerror(errno));
+	close(pid_file);
     return EXIT_FAILURE;
 }

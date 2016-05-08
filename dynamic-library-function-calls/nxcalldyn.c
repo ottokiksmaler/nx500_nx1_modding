b22055a4149eb4ff7c79b222c5ce7cdc0da5ed88
gitvasile <gitvasile@users.noreply.github.com>
2016-05-08 19:31:07 +0200

    v1.0

--- dynamic-library-function-calls/nxcalldyn.c
+++ dynamic-library-function-calls/nxcalldyn.c
@@ -1,92 +1,125 @@
+/*
+ * Copyright (C) 2016  Vasile Dumitrescu, (ppnx.vasile@dfgh.net)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * 
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <dlfcn.h>
 
+typedef void (*libFunc)();
 typedef int (*libFunc_iii)(int, int);
 typedef char *(*libFunc_si)(int);
+typedef void (*libFunc_vi)(int);
 
-typedef void (*callWrapper)(char *, char *, int, const char* []);
+// does not handle errors - returns 0 for malformed values
+unsigned int iDecHexToInt(const char *sStringVal) {
+	unsigned int retVal=0;
+	if(strstr(sStringVal, "0x")!=NULL) {
+		sscanf(sStringVal, "%x", &retVal);
+	}	else {
+		sscanf(sStringVal, "%d", &retVal);
+	}
+	return retVal;
+}
+
 	// int libfunc(int, int)
-void call_iii(char *libPath, char *funcName, int argCount, const char *argList[]) {
-	printf("sss\n");
+void call_iii(libFunc func, const char *libPath, const char *funcName, int argCount, const char *argList[]) {
 	if(argCount==2) {
-		int arg1=0; int arg2=0;
-		if(sscanf(argList[0], "%d", &arg1)==1 && sscanf(argList[1], "%d", &arg2)==1) {
-			printf("called %s: %s(%d [%#010x], %d [%#010x])\n", libPath, funcName, arg1, arg1, arg2, arg2);
-		} else {
-			printf("cannot interpret the arguments\n");
-		}
+		int arg1=iDecHexToInt(argList[0]);
+		int arg2=iDecHexToInt(argList[1]);
+		int retVal=((libFunc_iii)func)(arg1, arg2);
+		printf("called %s: %s(%d [0x%x], %d [0x%x]) ===> %d [0x%x]\n", libPath, funcName, arg1, arg1, arg2, arg2, retVal, retVal);
 	} else {
 		printf("this function needs exactly two integer arguments\n");
 	}
 }
 
 	// char *libfunc(int)
-void call_si(char *libPath, char *funcName, int argCount, const char *argList[]) {
+void call_si(libFunc func, const char *libPath, const char *funcName, int argCount, const char *argList[]) {
 	if(argCount==1) {
-		int arg1=0;
-		if(sscanf(argList[0], "%d", &arg1)==1) {
-			printf("called %s: %s(%d [%#010x])\n", libPath, funcName, arg1, arg1);
-		} else {
-			printf("cannot interpret the argument\n");
-		}
+		int arg1=iDecHexToInt(argList[0]);
+		char *retVal=((libFunc_si)func)(arg1);
+		printf("called %s: %s(%d [0x%x]) ===> %s\n", libPath, funcName, arg1, arg1, retVal);
 	} else {
 		printf("this function needs exactly one integer argument\n");
 	}
 }
 	// void libfunc(int)
-void call_vi(char *libPath, char *funcName, int argCount, const char *argList[]) {
+void call_vi(libFunc func, const char *libPath, const char *funcName, int argCount, const char *argList[]) {
 	if(argCount==1) {
-		int arg1=0;
-		if(sscanf(argList[0], "%d", &arg1)==1) {
-			printf("called %s: %s(%d [%#010x])\n", libPath, funcName, arg1, arg1);
-		} else {
-			printf("cannot interpret the argument\n");
-		}
+		int arg1=iDecHexToInt(argList[0]);
+		((libFunc_vi)func)(arg1);
+		printf("called %s: %s(%d [0x%x])\n", libPath, funcName, arg1, arg1);
 	} else {
 		printf("this function needs exactly one integer argument\n");
 	}
 }
+	// void libfunc()
+void call_vv(libFunc func, const char *libPath, const char *funcName, int argCount) {
+	if(argCount==0) {
+		func();
+		printf("called %s: %s()\n", libPath, funcName);
+	} else {
+		printf("this function needs no argument\n");
+	}
+}
 
-static struct {
-	char *sLibPath;
-	char *sFuncName;
-	callWrapper callFunc;
-	char *sFuncDesc;
-} functionList[] = {
-	{ "/usr/lib/libprefman.so", "pref_get_long",  &call_iii, "int pref_get_long(int, int)" },
-	{ "/usr/lib/libprefman.so", "pref_get_name",  &call_si,  "char *pref_get_name(int)"    },
-	{ "/usr/lib/libprefman.so", "pref_show_info", &call_vi,  "void pref_show_info(int)"    }
-};
 
-int call_library() {
-	void     *handle = NULL;
-	libFunc_iii func = NULL;
-	handle = dlopen("/usr/lib/libprefman.so", RTLD_LAZY | RTLD_GLOBAL);
+int call_library(const char *callType, const char *libPath, const char *funcName, int argCount, const char *argList[]) {
+	void *handle = NULL;
+	libFunc func = NULL;
+	handle = dlopen(libPath, RTLD_LAZY | RTLD_GLOBAL);
 	if (handle == NULL) {
-		fprintf(stderr, "Unable to open lib: %s\n", dlerror());
+		printf("Unable to open lib: %s\n", dlerror());
 		return -1;
 	}
-	*(void **) (&func) = dlsym(handle, "pref_get_long");
-
+	*(void **) (&func) = dlsym(handle, funcName);
 	if (func == NULL) {
-		fprintf(stderr, "Unable to get symbol\n");
+		printf("Unable to find function\n");
 		return -1;
 	}
-
-	printf("pref_get_long(3, 0x208) = %ld\n", (*func)(3, 0x208));
+  if(strcmp(callType, "iii")==0) {
+		call_iii(func, libPath, funcName, argCount, argList);
+	} else if(callType, "si") {
+		call_si(func, libPath, funcName, argCount, argList);
+	} else if(callType, "vi") {
+		call_vi(func, libPath, funcName, argCount, argList);
+	} else if(callType, "vv") {
+		call_vv(func, libPath, funcName, argCount);
+	}
 	return 0;
 }
 
+
+// calldyn calltype libpath funcname arg1 arg2 ...
 int main(int argc, const char *argv[]) {
-	int i;
-	int iFunctionListSize=sizeof(functionList)/sizeof(functionList[0]);
-	for(i=0; i<iFunctionListSize; i++) {
-		if(strcmp(functionList[i].sFuncName, argv[1])==0) {
-			functionList[i].callFunc(functionList[i].sLibPath, functionList[i].sFuncName, argc-2, &argv[2]);
-			break;
-		}
+	if(argc<=4) {
+		printf("NX camera dynamic function call - nxcalldyn v1.0\n");
+		printf("Copyright (C) 2016 Vasile Dumitrescu (ppnx.vasile@dfgh.net)\n");
+		printf("License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n");
+		printf("This is free software: you are free to change and redistribute it.\n");
+		printf("There is NO WARRANTY, to the extent permitted by law.\n");
+		printf("\n");
+		printf("Usage: nxcalldyn callType libPath funcName arg1 ...\n");
+		printf("  callType is one of iii, si, vi, vv\n");
+		printf("  libPath must be complete library name, including path, e.g. /usr/lib/libprefman.so\n");
+	} else {
+		call_library(argv[1], argv[2], argv[3], argc-4, &argv[4]);
 	}
-	if(i==iFunctionListSize) printf("function not supported\n");
 	return 0;
 }

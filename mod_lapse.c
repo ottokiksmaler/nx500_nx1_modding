344fee023dbca8ba454443a3aa40c0a57aad7caf
ottokiksmaler <ottokiksmaler@users.noreply.github.com>
2016-09-23 14:13:18 +0200

    Added EV Smoothing (day-to-night lapses)

--- mod_lapse.c
+++ mod_lapse.c
@@ -18,31 +18,154 @@
 #include <string.h>
 #include <sys/msg.h>
 #include <errno.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 720
 #define SCREEN_HEIGHT 480
 
+// default debug
 static int debug=1;
-static Eina_Bool is_video=EINA_FALSE, lcd_off=EINA_FALSE, lcd_show_capture=EINA_FALSE, after_gui=EINA_FALSE, after_off=EINA_FALSE, save_settings=EINA_FALSE;
+
+// for EV Smoothing
+static int Tv, ev_step=0, ev_rolling=1, ev_steps=5, threshold=3;
+
+static Eina_Bool is_video=EINA_FALSE, lcd_off=EINA_FALSE, lcd_show_capture=EINA_FALSE, after_gui=EINA_FALSE, after_off=EINA_FALSE, save_settings=EINA_FALSE, smooth_ev=EINA_FALSE;
 static Evas_Object *black, *win, *bg, *bg2, *box, *btn, *chk, *table, *entry_period, *entry_duration, *entry_shots, *status_display, *entry_start_delay;
-pthread_t timer_thread, force_update_thread;
+pthread_t timer_thread, force_update_thread, sleep_detect_thread, ev_smooth_thread;
 char *input_duration,*input_period, *input_shots, *input_start_delay;
 int button_height = 80, button_width = 360, button_number = 0;
 char stringline[255], label_entry[255], sample_text[255];
+static int ev_values[255];
 static char *version_model, *version_release, *lapse_command;
 Evas_Object *popup_win;
 Ecore_Timer *timer;
 
-static int a_type, a_number=3, a_lcd_black, a_duration, a_show_timer, a_after_gui, a_after_off, a_start_delay, a_save_settings, a_scroll_info=1;
+static int a_type, a_number=3, a_lcd_black, a_duration, a_show_timer, a_after_gui, a_after_off, a_start_delay, a_save_settings, a_scroll_info=1, a_ev_smooth=0;
 static float a_delay=3.0;
 
 static char *settings_file = "/root/mod_lapse.cfg";
 static char *capture_message = "app nx capture single";
 
+static void run_command(char *command)
+{
+	if (debug) printf("CMD: %s", command);
+	char *cmd;
+	asprintf(&cmd, "%s", command);
+	system(cmd);
+	free(cmd);
+	if (debug) printf(" done.\n");
+}
+
+long msec_passed(struct timeval *fromtime, struct timeval *totime)
+{
+  long msec;
+  msec=(totime->tv_sec-fromtime->tv_sec)*1000;
+  msec+=(totime->tv_usec-fromtime->tv_usec)/1000;
+  return msec;
+}
+
+// Detect sleep events
+void* timer_loop(void* arg) {
+	struct timeval previous_time_loop, current_time_loop;
+	int timer_sleep=1000;
+	long msec_elapsed;
+	while(1) {
+		gettimeofday(&previous_time_loop, NULL); 
+		usleep(timer_sleep*1000);
+		gettimeofday(&current_time_loop, NULL); 
+		msec_elapsed = msec_passed(&previous_time_loop,&current_time_loop);
+		if (msec_elapsed>(timer_sleep+100)) exit(1);
+	}
+}
+
+void ev_smooth_clean(int steps, int value) {
+	int i;
+	if (value==0) {
+		for(i=0;i<steps;i++) ev_values[i]=value;
+	} else {
+		for(i=0;i<steps;i++) ev_values[i]+=value;
+	}
+}
+
+void do_ev_smooth(int steps) {
+	int ev_get() {
+		FILE *fp;
+		char *spl = "";
+		int Ev=0;
+		
+		if (debug==2) {
+			fp = popen("cat ../exposure.txt", "r");
+		} else {
+			fp = popen("/bin/bash -c \"/usr/bin/st key push s1; /usr/bin/st key release s1; /usr/bin/st cap lockinfo\"", "r");
+		}
+		
+		if (fp == NULL) {
+			if (debug) printf("Failed to get current EV data\n");
+			return 0;
+		} else {
+			if ((fgets(stringline, sizeof(stringline) - 1, fp) != NULL) && (fgets(stringline, sizeof(stringline) - 1, fp) != NULL) &&(fgets(stringline, sizeof(stringline) - 1, fp) != NULL)) {
+				stringline[0] = '_';	// fix st output
+				spl = strtok(stringline, ":");
+				spl = strtok(NULL, ",");
+				Tv=atoi(spl);
+				if (fgets(stringline, sizeof(stringline) - 1, fp) != NULL) {
+					stringline[0] = '_';	// fix st output
+					spl = strtok(stringline, ":");
+					spl = strtok(NULL, " ");
+					Ev=atoi(spl);
+					if (debug) printf("Tv: %d\tEv: %d\n",Tv,Ev);
+				}
+			} else {
+				printf("Error reding from file. Stringline: %s\n",stringline);
+			}
+			pclose(fp);
+			usleep(500000);
+			if (debug==2) {
+				return 16-rand()%20;
+			}
+			return Ev;
+		}
+	}
+
+	int i, sum=0, deltaEv=0, newTv=0;
+	float avg=0.0;
+	char *cmd;
+	ev_values[ev_step]=ev_get();
+	for(i=0;i<steps;i++) {
+		sum+=ev_values[i];
+		printf("%d\t",ev_values[i]);
+	}
+	printf("\n");
+	avg=(float)sum/steps;
+	printf("AVG: %f\n",avg);
+	if ((abs(avg)+0.05)>threshold) {
+		sum=0;
+		for(i=0;i<steps;i++) {
+			if ((avg>0 && ev_values[i]>=threshold) || (avg<0 && ev_values[i]<=threshold)) sum++;
+		}
+		if (sum>(steps/2)) {
+			deltaEv = (int)round(avg/abs(avg))*threshold;
+			newTv = Tv + deltaEv;
+			printf("Adjusting shutter by %d to %d (%d points above %d)\n",deltaEv, newTv, sum, steps/2);
+			if (debug<2) {
+				asprintf(&cmd,"st cap capdtm setvar TV %d 4",newTv);
+				sleep(1);
+				run_command(cmd);
+			}
+			if (ev_rolling==1) ev_smooth_clean(steps, -deltaEv);
+			else ev_smooth_clean(steps, 0);
+		} 
+	}
+	ev_step++;
+	if (ev_step>=steps) {
+		ev_step=0;
+	}
+}
+
 int send_message(char * message_in){
 	int i=0, result, fd=-1;
 	unsigned char message_text[208]="\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-	unsigned char *param, *message, *spl;
+	char  *param, *message, *spl;
 	struct msg_buf_out {
 		long mtype;
 		char mtext[208];
@@ -55,7 +178,7 @@ int send_message(char * message_in){
 	spl=strtok(message," ");
 	
 	while(spl && strlen(spl)>0) {
-		if (debug) printf("Adding at %d %s(%d)\n",4+i*20,spl,(int)strlen(spl));
+		if (debug>2) printf("Adding at %d %s(%d)\n",4+i*20,spl,(int)strlen(spl));
 		memcpy(message_text+4+i*20,spl,strlen(spl));
 		i++;
 		spl = strtok(NULL, " ");
@@ -63,7 +186,7 @@ int send_message(char * message_in){
 	asprintf(&spl,"%d",i);
 	memcpy(message_text,spl, strlen(spl));
 
-	if (debug) {
+	if (debug>1) {
 		printf("Message:\n");
 		for(i=0;i<208;i++) {
 			if ((i-4)%20==0) printf("\n");
@@ -101,21 +224,11 @@ static void quit_app()
 
 }
 
-static void run_command(char *command)
-{
-	if (debug) printf("CMD: %s", command);
-	char *cmd;
-	asprintf(&cmd, "%s", command);
-	system(cmd);
-	free(cmd);
-	if (debug) printf(" done.\n");
-}
-
-static void do_save_settings(int is_video, int shots, float delay, int duration, int lcd_black, int lcd_show, int after_off, int after_gui, int start_delay)
+static void do_save_settings(int is_video, int shots, float delay, int duration, int lcd_black, int lcd_show, int after_off, int after_gui, int start_delay, int ev_smooth)
 {
 	FILE *f = fopen(settings_file, "w");
 	if (f != NULL) {
-		fprintf(f, "%d %d %f %d %d %d %d %d %d\n", is_video, shots, delay, duration, lcd_black, lcd_show, after_off, after_gui, start_delay);
+		fprintf(f, "%d %d %f %d %d %d %d %d %d %d\n", is_video, shots, delay, duration, lcd_black, lcd_show, after_off, after_gui, start_delay, ev_smooth);
 		fclose(f);
 	}
 }
@@ -131,11 +244,11 @@ static void do_load_settings()
 	if (fp != NULL) {
 		if (debug) printf("Reading configuration... ");
 		if ((read = getline(&line, &len, fp)) != -1) {
-			sscanf(line,  "%d %d %f %d %d %d %d %d %d", &a_type, &a_number, &a_delay, &a_duration, &a_lcd_black, &a_show_timer, &a_after_off, &a_after_gui, &a_start_delay);
+			sscanf(line,  "%d %d %f %d %d %d %d %d %d %d", &a_type, &a_number, &a_delay, &a_duration, &a_lcd_black, &a_show_timer, &a_after_off, &a_after_gui, &a_start_delay, &a_ev_smooth);
 		}
 		fclose(fp);
 		free(line);
-		if (debug) printf ("Loaded: %d %d %f %d %d %d %d %d %d\n", a_type, a_number, a_delay, a_duration, a_lcd_black, a_show_timer, a_after_off, a_after_gui, a_start_delay);
+		if (debug) printf ("Loaded: %d %d %f %d %d %d %d %d %d %d\n", a_type, a_number, a_delay, a_duration, a_lcd_black, a_show_timer, a_after_off, a_after_gui, a_start_delay, a_ev_smooth);
 	} else {
 		printf ("Cannot access %s configuration file.\n",settings_file);
 	}
@@ -243,11 +356,17 @@ void* force_update(void* arg) {
 }
 
 
-static void do_capture(int type, int number, float delay, int lcd_black, int duration, int show_timer, int after_off, int after_gui, int start_delay) {
+static void do_capture(int type, int number, float delay, int lcd_black, int duration, int show_timer, int after_off, int after_gui, int start_delay, int ev_smooth) {
 	int i;
 	char *status_message,*prefix;
-	printf("Capture Video: %d\t Number: %d\tDelay: %f\tDuration: %d\tWait: %d\tBlackout: %d\tTimer: %d\t After OFF:%d\tAfter GUI:%d\n",
-		   type, number, delay, duration, start_delay, lcd_black, show_timer, after_off, after_gui);
+	struct timeval previous_time, current_time;
+	long msec_elapsed, sleep_for;
+	
+	ev_smooth_clean(ev_steps,0);
+	
+	printf("Capture Video: %d\t Number: %d\tDelay: %f\tDuration: %d\tWait: %d\tBlackout: %d\tTimer: %d\t After OFF:%d\tAfter GUI:%d\tEV Smooth:%d\n",
+		   type, number, delay, duration, start_delay, lcd_black, show_timer, after_off, after_gui, ev_smooth);
+	pthread_create(&sleep_detect_thread, NULL, &timer_loop, NULL);
 	if (0!=type) run_command("/usr/bin/st key click del");
 	if (delay<0.99) delay=1.0;
 	if (1==lcd_black) {
@@ -271,6 +390,7 @@ static void do_capture(int type, int number, float delay, int lcd_black, int dur
 	}
 	asprintf(&prefix," ");
 	for (i=0;i<number;i++) {
+		gettimeofday(&previous_time, NULL); 
 		if (i % 10 > 0 && a_scroll_info==1) 
 			asprintf(&prefix,"<br>%s",prefix);
 		else 
@@ -294,7 +414,14 @@ static void do_capture(int type, int number, float delay, int lcd_black, int dur
 			elm_object_text_set(status_display,status_message);
 			pthread_create(&force_update_thread, NULL, &force_update, NULL);
 		}
-		if (i < (number-1)) usleep(delay*1000000); else if (show_timer) {
+		if (i < (number-1)) {
+			if (ev_smooth) do_ev_smooth(ev_steps);
+			gettimeofday(&current_time, NULL); 
+			msec_elapsed = msec_passed(&previous_time,&current_time);
+			sleep_for=delay*1000000-msec_elapsed*1000;
+			if (sleep_for<0) sleep_for=0;
+			usleep(sleep_for); 
+		} else if (show_timer) {
 			asprintf(&status_message,"%s Lapse complete %d/%d",prefix,i+1,number);
 			elm_object_text_set(status_display,status_message);
 			pthread_create(&force_update_thread, NULL, &force_update, NULL);
@@ -319,8 +446,10 @@ static void click_save_settings(void *data, Evas_Object *obj, void *event_info)
 	start_delay=atoi(elm_object_text_get(entry_start_delay));
 	lcd_black=(int)lcd_off;
 	lcd_show=(int)lcd_show_capture;
+	//ev_smooth=atoi(elm_object_text_get(entry_ev_smooth));
+	int ev_smooth=0;
 	printf("Saving settings...");
-	do_save_settings(video, shots, delay, duration, lcd_black, lcd_show, (int)after_off, (int)after_gui, (int)start_delay);
+	do_save_settings(video, shots, delay, duration, lcd_black, lcd_show, (int)after_off, (int)after_gui, (int)start_delay, (int)ev_smooth);
 	printf(" done.\n");
 }
 
@@ -338,10 +467,10 @@ static void start_capture(void *data, Evas_Object *obj, void *event_info)
 	lcd_black=(int)lcd_off;
 	lcd_show=(int)lcd_show_capture;
 	if (0==video) {
-		asprintf(&command, "%s photo %d %f %d %d %d %d %d", lapse_command, shots, delay, lcd_black, lcd_show, (int)after_off, (int)after_gui, (int)start_delay);
+		asprintf(&command, "%s photo %d %f %d %d %d %d %d %d", lapse_command, shots, delay, lcd_black, lcd_show, (int)after_off, (int)after_gui, (int)start_delay, (int)smooth_ev);
 	}
 	if (1==video) {
-		asprintf(&command, "%s video %d %f %d %d %d %d %d %d", lapse_command, shots, delay, duration, lcd_black, lcd_show, (int)after_off, (int)after_gui, (int)start_delay);
+		asprintf(&command, "%s video %d %f %d %d %d %d %d %d %d", lapse_command, shots, delay, duration, lcd_black, lcd_show, (int)after_off, (int)after_gui, (int)start_delay, (int)smooth_ev);
 	}
 	run_command(command);
 	if (1==after_gui) {
@@ -678,10 +807,10 @@ void show_main()
 	evas_object_show(chk);
 	evas_object_size_hint_min_set(chk, button_width,
 						button_height);
-	elm_check_state_pointer_set(chk, &after_gui);
+	elm_check_state_pointer_set(chk, &smooth_ev);
 	btn = elm_button_add(win);
 	elm_object_style_set(btn, "transparent");
-	elm_object_text_set(btn, "GUI after lapse");
+	elm_object_text_set(btn, "Smooth EV");
 	evas_object_show(btn);
 	evas_object_size_hint_min_set(btn, button_width, button_height);
 	bg2 = evas_object_rectangle_add(evas_object_evas_get(btn));
@@ -691,12 +820,12 @@ void show_main()
 	bg = evas_object_rectangle_add(evas_object_evas_get(btn));
 	evas_object_size_hint_min_set(bg, button_width - 2,
 						button_height - 2);
-	evas_object_color_set(bg, 0, 0, 0, 255);
+	evas_object_color_set(bg, 60, 80, 120, 255);
 	evas_object_show(bg);
 	elm_table_pack(table, bg2, col, row, 1, 1);
 	elm_table_pack(table, bg, col, row, 1, 1);
-//	elm_table_pack(table, btn, col, row, 1, 1);
-//	elm_table_pack(table, chk, col, row, 1, 1);
+	elm_table_pack(table, btn, col, row, 1, 1);
+	elm_table_pack(table, chk, col, row, 1, 1);
 	// END BUTTON
 
 	
@@ -757,7 +886,7 @@ void show_main()
 
 void* timer_capture(void* arg) {
 // 	sleep(1);
-	do_capture(a_type,a_number,a_delay,a_lcd_black,a_duration,a_show_timer, a_after_off, a_after_gui, a_start_delay);
+	do_capture(a_type,a_number,a_delay,a_lcd_black,a_duration,a_show_timer, a_after_off, a_after_gui, a_start_delay, a_ev_smooth);
 	return NULL;
 }
 
@@ -788,6 +917,7 @@ EAPI int elm_main(int argc, char **argv)
 			a_after_off = atoi(argv[6]);
 			a_after_gui = atoi(argv[7]);
 			a_start_delay = atoi(argv[8]);
+			a_ev_smooth = atoi(argv[9]);
 			a_duration=0;
 			pthread_create(&timer_thread, NULL, &timer_capture, NULL);
 			if (1==a_lcd_black || 1==a_show_timer) show_black();
@@ -802,6 +932,7 @@ EAPI int elm_main(int argc, char **argv)
 			a_after_off = atoi(argv[7]);
 			a_after_gui = atoi(argv[8]);
 			a_start_delay = atoi(argv[9]);
+			a_ev_smooth = atoi(argv[10]);
 			pthread_create(&timer_thread, NULL, &timer_capture, NULL);
 			if (1==a_lcd_black || 1==a_show_timer) show_black();
 		}
